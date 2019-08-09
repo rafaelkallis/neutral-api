12c2d6bf04ca85ec2b28a1db6713baba91f10731
Rafael Kallis <rk@rafaelkallis.com>
2019-08-09 19:04:15 +0200

    feat: added role controller stubs

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, NotImplementedException } from '@nestjs/common';
+import { Role } from '../common';
 
 @Controller('roles')
-export class RoleController {}
+export class RoleController {
+  getRoles(): Promise<Role[]> {
+    throw new NotImplementedException();
+  }
+
+  getRole(): Promise<Role> {
+    throw new NotImplementedException();
+  }
+
+  createRole(): Promise<Role> {
+    throw new NotImplementedException();
+  }
+
+  patchRole(): Promise<Role> {
+    throw new NotImplementedException();
+  }
+
+  deleteRole(): Promise<Role> {
+    throw new NotImplementedException();
+  }
+}
