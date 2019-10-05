482e5cbc2ac24a385b99fd405f01dff3f7b5632f
Rafael Kallis <rk@rafaelkallis.com>
2019-10-05 17:03:47 -0500

    refactor: removed unused imports and constant

--- src/common/services/random.service.ts
+++ src/common/services/random.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import base64url from 'base64url';
-import crypto from 'crypto';
 import ObjectId from 'bson-objectid';
 
 /**
@@ -8,8 +6,6 @@ import ObjectId from 'bson-objectid';
  */
 @Injectable()
 export class RandomService {
-  private static readonly N_SALT_BYTES = 16;
-
   /**
    * Generates a url-safe, ordered, random string.
    * Id generation scheme based on MongoDB's ObjectId.
