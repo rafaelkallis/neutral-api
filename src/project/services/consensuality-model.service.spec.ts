dff7cde9e48ea629b285c5ca1c55144fac2a1c4a
Rafael Kallis <rk@rafaelkallis.com>
2019-12-10 21:34:45 +0100

    feat: variance based consensuality method

--- src/project/services/consensuality-model.service.spec.ts
+++ src/project/services/consensuality-model.service.spec.ts
@@ -13,136 +13,76 @@ describe('ConsensualityModelService', () => {
     consensualityModelService = module.get(ConsensualityModelService);
   });
 
+  const cyclePeerReviews = {
+    a: { a: 0, b: 1, c: 0, d: 0 },
+    b: { a: 0, b: 0, c: 1, d: 0 },
+    c: { a: 0, b: 0, c: 0, d: 1 },
+    d: { a: 1, b: 0, c: 0, d: 0 },
+  };
+
+  const clusterPeerReviews = {
+    a: { a: 0, b: 1, c: 0, d: 0 },
+    b: { a: 1, b: 0, c: 1, d: 0 },
+    c: { a: 0, b: 0, c: 0, d: 1 },
+    d: { a: 0, b: 0, c: 1, d: 0 },
+  };
+
+  const oneDidItAllPeerReviews = {
+    a: { a: 0, b: 0, c: 0, d: 1 },
+    b: { a: 0, b: 0, c: 0, d: 1 },
+    c: { a: 0, b: 0, c: 0, d: 1 },
+    d: { a: 1 / 3, b: 1 / 3, c: 1 / 3, d: 0 },
+  };
+
   test('should use mean deviation method for computing consensuality', () => {
     jest.spyOn(consensualityModelService, 'meanDeviationMethod');
-    const peerReviews = {
-      a: {
-        a: 0,
-        b: 1,
-        c: 0,
-        d: 0,
-      },
-      b: {
-        a: 0,
-        b: 0,
-        c: 1,
-        d: 0,
-      },
-      c: {
-        a: 0,
-        b: 0,
-        c: 0,
-        d: 1,
-      },
-      d: {
-        a: 1,
-        b: 0,
-        c: 0,
-        d: 0,
-      },
-    };
     const consensuality = consensualityModelService.computeConsensuality(
-      peerReviews,
+      cyclePeerReviews,
     );
     expect(consensuality).toBeGreaterThanOrEqual(0);
     expect(consensuality).toBeLessThanOrEqual(1);
     expect(consensualityModelService.meanDeviationMethod).toHaveBeenCalledWith(
-      peerReviews,
+      cyclePeerReviews,
     );
   });
 
   describe('mean deviation method', () => {
     test('cycle', () => {
-      const peerReviews = {
-        a: {
-          a: 0,
-          b: 1,
-          c: 0,
-          d: 0,
-        },
-        b: {
-          a: 0,
-          b: 0,
-          c: 1,
-          d: 0,
-        },
-        c: {
-          a: 0,
-          b: 0,
-          c: 0,
-          d: 1,
-        },
-        d: {
-          a: 1,
-          b: 0,
-          c: 0,
-          d: 0,
-        },
-      };
-      const c = consensualityModelService.meanDeviationMethod(peerReviews);
+      const c = consensualityModelService.meanDeviationMethod(cyclePeerReviews);
       expect(c).toBeCloseTo(0);
     });
 
     test('clusters', () => {
-      const peerReviews = {
-        a: {
-          a: 0,
-          b: 1,
-          c: 0,
-          d: 0,
-        },
-        b: {
-          a: 1,
-          b: 0,
-          c: 0,
-          d: 0,
-        },
-        c: {
-          a: 0,
-          b: 0,
-          c: 0,
-          d: 1,
-        },
-        d: {
-          a: 0,
-          b: 0,
-          c: 1,
-          d: 0,
-        },
-      };
-      const c = consensualityModelService.meanDeviationMethod(peerReviews);
+      const c = consensualityModelService.meanDeviationMethod(
+        clusterPeerReviews,
+      );
       expect(c).toBeCloseTo(0);
     });
 
     test('one did everything', () => {
-      const peerReviews = {
-        a: {
-          a: 0,
-          b: 0,
-          c: 0,
-          d: 1,
-        },
-        b: {
-          a: 0,
-          b: 0,
-          c: 0,
-          d: 1,
-        },
-        c: {
-          a: 0,
-          b: 0,
-          c: 0,
-          d: 1,
-        },
-        d: {
-          a: 1 / 3,
-          b: 1 / 3,
-          c: 1 / 3,
-          d: 0,
-        },
-      };
-      const c = consensualityModelService.meanDeviationMethod(peerReviews);
+      const c = consensualityModelService.meanDeviationMethod(
+        oneDidItAllPeerReviews,
+      );
       expect(c).toBeCloseTo(3 / 4);
+    });
+  });
+
+  describe('variance method', () => {
+    test('cycle', () => {
+      const c = consensualityModelService.varianceMethod(cyclePeerReviews);
+      expect(c).toBeCloseTo(0);
+    });
+
+    test('clusters', () => {
+      const c = consensualityModelService.varianceMethod(clusterPeerReviews);
+      expect(c).toBeCloseTo(0);
+    });
+
+    test('one did everything', () => {
+      const c = consensualityModelService.varianceMethod(
+        oneDidItAllPeerReviews,
+      );
+      expect(c).toBeCloseTo(0.91666);
     });
   });
 });
--- src/project/services/consensuality-model.service.ts
+++ src/project/services/consensuality-model.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
+function sum(arr: number[]): number {
+  return arr.reduce((a, b) => a + b);
+}
+
+function mean(arr: number[]): number {
+  return arr.reduce((a, b) => a + b) / arr.length;
+}
+
 /* eslint-disable security/detect-object-injection */
 
 /**
@@ -55,23 +63,18 @@ export class ConsensualityModelService {
    *                   maxCMD
    *
    *
-   *  5) non consensuality
+   *  5) consensuality
    *
-   *                   sum_j normCMD(j)
-   *    nonConsens := ------------------ , card(j) == n
-   *                         n
+   *                    sum_j normCMD(j)
+   *    consens := 1 - ------------------ , card(j) == n
+   *                           n
    *
-   *
-   *  6) consensuality
-   *
-   *    consens := 1 - nonConsens
    */
   public meanDeviationMethod(
     peerReviews: Record<string, Record<string, number>>,
   ): number {
     const peers = Object.keys(peerReviews);
     const n = peers.length;
-    const sum = (arr: number[]): number => arr.reduce((a, b) => a + b);
     function columnMean(j: string): number {
       return (
         sum(peers.filter(i => i !== j).map(i => peerReviews[i][j])) / (n - 1)
@@ -85,9 +88,67 @@ export class ConsensualityModelService {
       );
     }
     const maxColumnMeanDeviation = (2 * (n - 2)) / (n - 1);
-    const nonConsenuality =
-      sum(peers.map(j => columnMeanDeviation(j) / maxColumnMeanDeviation)) / n;
+    const nonConsenuality = mean(
+      peers.map(j => columnMeanDeviation(j) / maxColumnMeanDeviation),
+    );
     return 1 - nonConsenuality;
+  }
+
+  /**
+   * Computes consensuality using statistical variance.
+   *
+   * Given peer review matrix M (n x n),
+   *  let M_{ij} be the review of i over j,
+   *  where M_{ii} = 0,
+   *  then let:
+   *
+   *  1) column variance
+   *
+   *    var(j)
+   *
+   *
+   *  2) max column variance
+   *
+   *    maxVar := var([1, 0, ..., 0]) , where card([1, 0, ..., 0]) == n - 1
+   *
+   *
+   *  3) normalized column variance
+   *
+   *                   var(j)
+   *    normVar(j) := --------
+   *                   maxVar
+   *
+   *
+   *  4) consensuality
+   *
+   *                    sum_j normVar(j)
+   *    consens := 1 - ------------------ , card(j) == n
+   *                           n
+   */
+  public varianceMethod(
+    peerReviews: Record<string, Record<string, number>>,
+  ): number {
+    const peers = Object.keys(peerReviews);
+    const n = peers.length;
+    function variance(arr: number[]): number {
+      const n = arr.length;
+      const mean = sum(arr) / n;
+      const squaredSum = sum(arr.map(x => Math.pow(x - mean, 2)));
+      return squaredSum / n;
+    }
+    function worstColumn(n: number): number[] {
+      const arr = new Array(n);
+      arr.fill(0);
+      arr[0] = 1;
+      return arr;
+    }
+    function column(j: string): number[] {
+      return peers.filter(i => i !== j).map(i => peerReviews[i][j]);
+    }
+    return (
+      1 -
+      mean(peers.map(j => variance(column(j)) / variance(worstColumn(n - 1))))
+    );
   }
 
   public isConsensual(consensuality: number): boolean {
