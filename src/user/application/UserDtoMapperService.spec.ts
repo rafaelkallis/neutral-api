e35c6de25853170454a5e6877cf25f8979315b5a
Rafael Kallis <rk@rafaelkallis.com>
2020-03-15 19:22:22 +0100

    test: added avatarUrl in test

--- src/user/application/UserDtoMapperService.spec.ts
+++ src/user/application/UserDtoMapperService.spec.ts
@@ -2,17 +2,23 @@ import { User } from 'user/domain/User';
 import { ModelFaker } from 'test/ModelFaker';
 import { UserDtoMapperService } from 'user/application/UserDtoMapperService';
 import { MockConfigService } from 'config/infrastructure/MockConfigService';
+import { Avatar } from 'user/domain/value-objects/Avatar';
+import { PrimitiveFaker } from 'test/PrimitiveFaker';
 
 describe('user dto mapper', () => {
   let userDtoMapper: UserDtoMapperService;
+  let primitiveFaker: PrimitiveFaker;
   let modelFaker: ModelFaker;
   let user: User;
 
   beforeEach(async () => {
     const config = new MockConfigService();
+    config.set('SERVER_URL', 'http://example.com');
     userDtoMapper = new UserDtoMapperService(config);
+    primitiveFaker = new PrimitiveFaker();
     modelFaker = new ModelFaker();
     user = modelFaker.user();
+    user.avatar = Avatar.from(primitiveFaker.id());
   });
 
   test('general', () => {
@@ -22,7 +28,7 @@ describe('user dto mapper', () => {
       email: user.email.value,
       firstName: user.name.first,
       lastName: user.name.last,
-      avatarUrl: null,
+      avatarUrl: 'http://example.com/users/' + user.id.value + '/avatar',
       createdAt: user.createdAt.value,
       updatedAt: user.updatedAt.value,
     });
