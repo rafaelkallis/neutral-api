19cdc971f6617da909b25e5f36dbcc2acacfd2c4
Rafael Kallis <rk@rafaelkallis.com>
2020-05-08 15:17:10 +0200

    feat(environment): added environment

--- src/shared/utility/UtilityModule.ts
+++ src/shared/utility/UtilityModule.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ServiceExplorer } from 'shared/utility/application/ServiceExplorer';
 import { InvocationProxy } from 'shared/utility/application/InvocationProxy';
+import { Environment } from 'shared/utility/application/Environment';
 
 /**
  * Utility Module
  */
 @Module({
-  providers: [ServiceExplorer, InvocationProxy],
-  exports: [ServiceExplorer, InvocationProxy],
+  providers: [ServiceExplorer, InvocationProxy, Environment],
+  exports: [ServiceExplorer, InvocationProxy, Environment],
 })
 export class UtilityModule {}
