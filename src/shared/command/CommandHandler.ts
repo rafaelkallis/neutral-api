b689b8133b3c6eb2cc38f761621558d7e6ac66e0
Rafael Kallis <rk@rafaelkallis.com>
2020-05-31 14:02:19 +0200

    refactor(request handlers): got rid of @CommandHandler() and @QueryHandler()

--- src/shared/command/CommandHandler.ts
+++ src/shared/command/CommandHandler.ts
@@ -1,23 +1,5 @@
 import { Command } from 'shared/command/Command';
-import {
-  AbstractRequestHandler,
-  RequestHandler,
-} from 'shared/mediator/RequestHandler';
-import { Type, ScopeOptions } from '@nestjs/common';
-
-export function CommandHandler(
-  commandType: Type<Command<unknown>>,
-  scopeOptions?: ScopeOptions,
-): ClassDecorator {
-  return function (target: Function): void {
-    if (!(target.prototype instanceof AbstractCommandHandler)) {
-      throw new Error(
-        `${target.name} is not a command handler, did you extend ${AbstractCommandHandler.name} ?`,
-      );
-    }
-    RequestHandler(commandType, scopeOptions)(target);
-  };
-}
+import { AbstractRequestHandler } from 'shared/mediator/RequestHandler';
 
 export abstract class AbstractCommandHandler<
   T,
--- src/shared/query/QueryHandler.ts
+++ src/shared/query/QueryHandler.ts
@@ -1,23 +1,5 @@
-import {
-  AbstractRequestHandler,
-  RequestHandler,
-} from 'shared/mediator/RequestHandler';
-import { Type, ScopeOptions } from '@nestjs/common';
+import { AbstractRequestHandler } from 'shared/mediator/RequestHandler';
 import { Query } from 'shared/query/Query';
-
-export function QueryHandler(
-  queryType: Type<Query<unknown>>,
-  scopeOptions?: ScopeOptions,
-): ClassDecorator {
-  return function (target: Function): void {
-    if (!(target.prototype instanceof AbstractQueryHandler)) {
-      throw new Error(
-        `${target.name} is not a query handler, did you extend ${AbstractQueryHandler.name} ?`,
-      );
-    }
-    RequestHandler(queryType, scopeOptions)(target);
-  };
-}
 
 export abstract class AbstractQueryHandler<
   T,
