34404c8667f4aab580784208432617fa18c92708
Rafael Kallis <rk@rafaelkallis.com>
2020-06-14 16:45:45 +0200

    refactor(notification domain event handlers): got rid of memory repos in unit tests

--- src/notification/application/NotificationDomainEventHandlers.spec.ts
+++ src/notification/application/NotificationDomainEventHandlers.spec.ts
@@ -1,108 +1,133 @@
+import td from 'testdouble';
 import { NotificationDomainEventHandlers } from 'notification/application/NotificationDomainEventHandlers';
 import { NotificationFactoryService } from 'notification/domain/NotificationFactoryService';
-import { NotificationType } from 'notification/domain/value-objects/NotificationType';
+import { Notification } from 'notification/domain/Notification';
 import { ProjectPeerReviewStartedEvent } from 'project/domain/events/ProjectPeerReviewStartedEvent';
 import { ProjectManagerReviewStartedEvent } from 'project/domain/events/ProjectManagerReviewStartedEvent';
 import { ProjectFinishedEvent } from 'project/domain/events/ProjectFinishedEvent';
-import { ModelFaker } from 'test/ModelFaker';
 import { NotificationRepository } from 'notification/domain/NotificationRepository';
-import { MemoryNotificationRepository } from 'notification/infrastructure/MemoryNotificationRepository';
 import { UserAssignedEvent } from 'project/domain/events/UserAssignedEvent';
+import { UnitTestScenario } from 'test/UnitTestScenario';
 import { UserCollection } from 'user/domain/UserCollection';
 
-describe('notification sagas', () => {
-  let modelFaker: ModelFaker;
+describe(NotificationDomainEventHandlers.name, () => {
+  let scenario: UnitTestScenario<NotificationDomainEventHandlers>;
+  let notificationDomainEventHandler: NotificationDomainEventHandlers;
   let notificationRepository: NotificationRepository;
   let notificationFactory: NotificationFactoryService;
-  let notificationDomainEventHandler: NotificationDomainEventHandlers;
+  let notification: Notification;
 
-  beforeEach(() => {
-    modelFaker = new ModelFaker();
-    notificationRepository = new MemoryNotificationRepository();
-    notificationFactory = new NotificationFactoryService();
-    notificationDomainEventHandler = new NotificationDomainEventHandlers(
-      notificationRepository,
-      notificationFactory,
-    );
-    jest.spyOn(notificationRepository, 'persist');
-  });
-
-  test('should be defined', () => {
-    expect(notificationDomainEventHandler).toBeDefined();
+  beforeEach(async () => {
+    scenario = await UnitTestScenario.builder(NotificationDomainEventHandlers)
+      .addProviderMock(NotificationRepository)
+      .addProviderMock(NotificationFactoryService)
+      .build();
+    notificationDomainEventHandler = scenario.subject;
+    notificationRepository = scenario.module.get(NotificationRepository);
+    notificationFactory = scenario.module.get(NotificationFactoryService);
+    notification = td.object();
   });
 
   test('existing user assigned', async () => {
-    const owner = modelFaker.user();
-    const project = modelFaker.project(owner.id);
-    const assignee = modelFaker.user();
-    const role = modelFaker.role(assignee.id);
+    const owner = scenario.modelFaker.user();
+    const project = scenario.modelFaker.project(owner.id);
+    const assignee = scenario.modelFaker.user();
+    const role = scenario.modelFaker.role(assignee.id);
     const event = new UserAssignedEvent(project, role, assignee);
+    td.when(
+      notificationFactory.createNewAssignmentNotification(project, role),
+    ).thenReturn(notification);
 
     await notificationDomainEventHandler.onUserAssignedCreateNewAssignmentNotification(
       event,
     );
 
-    expect(notificationRepository.persist).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: NotificationType.NEW_ASSIGNMENT,
-      }),
-    );
+    td.verify(notificationRepository.persist(notification));
   });
 
   test('peer review requested', async () => {
-    const owner = modelFaker.user();
-    const project = modelFaker.project(owner.id);
-    const assignees = [modelFaker.user(), modelFaker.user(), modelFaker.user()];
+    const owner = scenario.modelFaker.user();
+    const project = scenario.modelFaker.project(owner.id);
+    const assignees = [
+      scenario.modelFaker.user(),
+      scenario.modelFaker.user(),
+      scenario.modelFaker.user(),
+    ];
     project.roles.addAll([
-      modelFaker.role(assignees[0].id),
-      modelFaker.role(assignees[1].id),
-      modelFaker.role(assignees[2].id),
+      scenario.modelFaker.role(assignees[0].id),
+      scenario.modelFaker.role(assignees[1].id),
+      scenario.modelFaker.role(assignees[2].id),
     ]);
     const event = new ProjectPeerReviewStartedEvent(
       project,
       new UserCollection(assignees),
     );
+    td.when(
+      notificationFactory.createPeerReviewRequestedNotification(
+        project,
+        td.matchers.anything(),
+      ),
+    ).thenReturn(notification);
 
     await notificationDomainEventHandler.onPeerReviewStartedCreatePeerReviewRequestedNotifications(
       event,
     );
 
-    expect(notificationRepository.persist).toHaveBeenCalled();
+    td.verify(
+      notificationRepository.persist(notification, notification, notification),
+    );
     // TODO: more assertions
   });
 
   test('manager review requested', async () => {
-    const owner = modelFaker.user();
-    const project = modelFaker.project(owner.id);
+    const owner = scenario.modelFaker.user();
+    const project = scenario.modelFaker.project(owner.id);
     const event = new ProjectManagerReviewStartedEvent(project);
+    td.when(
+      notificationFactory.createManagerReviewRequestedNotification(project),
+    ).thenReturn(notification);
 
     await notificationDomainEventHandler.onManagerReviewStartedCreateManagerReviewRequestedNotification(
       event,
     );
 
-    expect(notificationRepository.persist).toHaveBeenCalledWith(
-      expect.objectContaining({
-        type: NotificationType.MANAGER_REVIEW_REQUESTED,
-      }),
-    );
+    td.verify(notificationRepository.persist(notification));
   });
 
   test('project finished', async () => {
-    const owner = modelFaker.user();
-    const project = modelFaker.project(owner.id);
-    const assignees = [modelFaker.user(), modelFaker.user(), modelFaker.user()];
+    const owner = scenario.modelFaker.user();
+    const project = scenario.modelFaker.project(owner.id);
+    const assignees = [
+      scenario.modelFaker.user(),
+      scenario.modelFaker.user(),
+      scenario.modelFaker.user(),
+    ];
     project.roles.addAll([
-      modelFaker.role(assignees[0].id),
-      modelFaker.role(assignees[1].id),
-      modelFaker.role(assignees[2].id),
+      scenario.modelFaker.role(assignees[0].id),
+      scenario.modelFaker.role(assignees[1].id),
+      scenario.modelFaker.role(assignees[2].id),
     ]);
     const event = new ProjectFinishedEvent(project);
+
+    td.when(
+      notificationFactory.createProjectFinishedNotification(
+        event.project,
+        td.matchers.anything(),
+      ),
+    ).thenReturn(notification);
 
     await notificationDomainEventHandler.onProjectFinishedCreateProjectFinishedNotification(
       event,
     );
 
-    expect(notificationRepository.persist).toHaveBeenCalled();
+    td.verify(
+      notificationRepository.persist(
+        notification,
+        notification,
+        notification,
+        notification,
+      ),
+    );
     // TODO: more assertions
   });
 });
