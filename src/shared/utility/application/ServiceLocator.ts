c35da2bfa8ad1eaffce5962d60aeedba0be1605a
Rafael Kallis <rk@rafaelkallis.com>
2020-06-14 16:32:19 +0200

    refactor(service locator): pinned to singleton scope

--- src/shared/utility/application/ServiceLocator.ts
+++ src/shared/utility/application/ServiceLocator.ts
@@ -1,20 +1,16 @@
-import { Injectable, Abstract, Type, Inject } from '@nestjs/common';
-import { ModuleRef, REQUEST, ContextId, ContextIdFactory } from '@nestjs/core';
-import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+import { ModuleRef } from '@nestjs/core';
+import { Class } from 'shared/domain/Class';
 
 @Injectable()
 export class ServiceLocator {
   private readonly moduleRef: ModuleRef;
-  private readonly contextId: ContextId;
 
-  public constructor(moduleRef: ModuleRef, @Inject(REQUEST) request: Request) {
+  public constructor(moduleRef: ModuleRef) {
     this.moduleRef = moduleRef;
-    this.contextId = ContextIdFactory.getByRequest(request);
   }
 
-  public async getService<T>(type: Abstract<T> | Type<T>): Promise<T> {
-    return this.moduleRef.resolve(type as any, this.contextId, {
-      strict: false,
-    });
+  public async getService<T>(type: Class<T>): Promise<T> {
+    return await this.moduleRef.get(type as any, { strict: false });
   }
 }
