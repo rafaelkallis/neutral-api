dde527da7ba6e95f1a5a7eb34af60a72cdd07db9
Rafael Kallis <rk@rafaelkallis.com>
2020-07-17 23:41:43 +0200

    feat(project): normalized peer review scores

--- src/project/domain/peer-review/value-objects/PeerReviewScore.ts
+++ src/project/domain/peer-review/value-objects/PeerReviewScore.ts
@@ -1,6 +1,7 @@
-import { UnitDecimalValueObject } from 'shared/domain/value-objects/UnitDecimalValueObject';
+import { NumberValueObject } from 'shared/domain/value-objects/NumberValueObject';
+import { DomainException } from 'shared/domain/exceptions/DomainException';
 
-export class PeerReviewScore extends UnitDecimalValueObject {
+export class PeerReviewScore extends NumberValueObject {
   public static of(value: number): PeerReviewScore {
     // if value in [-eps, eps] then value = eps
     if (Math.abs(value) <= Number.EPSILON) {
@@ -9,6 +10,12 @@ export class PeerReviewScore extends UnitDecimalValueObject {
     // if value in [1-eps, 1+eps] then value = 1-eps
     if (Math.abs(1 - value) <= Number.EPSILON) {
       value = 1 - Number.EPSILON;
+    }
+    if (value < Number.EPSILON || value >= Number.MAX_SAFE_INTEGER / 1e3) {
+      throw new DomainException(
+        'invalid_peer_review_score',
+        'Invalid peer-review score',
+      );
     }
     return new PeerReviewScore(value);
   }
